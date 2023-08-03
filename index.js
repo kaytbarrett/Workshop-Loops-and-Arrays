e4658e9bc274ca2119176d3850ecbd0fde8ebbe5
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-08-03 14:20:00 -0600

    Completed problem 2

--- index.js
+++ index.js
@@ -25,6 +25,35 @@ for (let i = 0; i < numArray.length; i++) {
 console.log(newArray)
 
 //Problem #2
+//Write a script that accepts a string of lowercase letters and have it print out how many consonants and vowels that string has
+
+//For example, if your string is dog, that would print out 2 consonants and 1 vowel
+//To do this, I can use a for loop for check each index of the string for whether or not that value is a consonant or a vowel
+//I can use an if/else statement to determine what to do if the letter if a consonant or a vowel
+
+//To test this code, uncomment out the line you wish to test
+//Be sure to check that the other lines are commented out!
+
+let string = "hello";
+// let string = "ukelele";
+// let string = "awesome";
+// let string = "onomonopia";
+// let string = "textbook";
+
+let vowelCount = 0;
+let consonantCount = 0;
+
+for (let i = 0; i < string.length; i++) {
+    if (string[i] === "a" || string[i] === "e" ||
+    string[i] === "i" || string[i] === "o" || string[i] === "u") {
+        vowelCount++;
+    } else {
+        consonantCount++;
+    }
+}
+
+console.log(string + " has " + consonantCount + " consonants and " + vowelCount + " vowels")
+
 
 //Problem #3
 
